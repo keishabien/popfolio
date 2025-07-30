e14063158fdab2f1ccc2937f93e6d0609d353983
keishabien <keisha@bmediallc.com>
2025-07-30 16:18:22 -0500

    test: add custom notice that plugin is active

--- popfolio.php
+++ popfolio.php
@@ -18,10 +18,23 @@ add_action( 'init', 'pop_setup_post_type' );
 
 function pop_activate() {
 	pop_setup_post_type();
+	$notices= get_option('pop_deferred_admin_notices', array());
+	$notices[]= "My Plugin: Custom Activation Message";
+	update_option('pop_deferred_admin_notices', $notices);
 	flush_rewrite_rules();
 }
 register_activation_hook( __FILE__, 'pop_activate' );
 
+
+add_action('admin_notices', 'pop_admin_notices');
+function pop_admin_notices() {
+	if ($notices= get_option('pop_deferred_admin_notices')) {
+		foreach ($notices as $notice) {
+			echo "<div class='updated'><p>$notice</p></div>";
+		}
+		delete_option('pop_deferred_admin_notices');
+	}
+}
 
 function pop_deactivate() {
 	unregister_post_type( 'portfolio' );
