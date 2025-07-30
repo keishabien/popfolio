aac2c8d9cfa9c7bde975d0d0ea90433c18293431
keishabien <keisha@bmediallc.com>
2025-07-30 16:07:05 -0500

    add: exit if accessed directly

--- popfolio.php
+++ popfolio.php
@@ -1,4 +1,9 @@
 <?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
 /**
  * Plugin Name: Popfolio
  * Description: A poppy portfolio
@@ -10,3 +15,9 @@ function pop_setup_post_type() {
 	register_post_type( 'portfolio', ['public' => true] );
 }
 add_action( 'init', 'pop_setup_post_type' );
+
+function pop_activate() {
+	pop_setup_post_type();
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'pop_activate' );
